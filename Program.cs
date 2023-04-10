0d1329dde6876f72766b9430949ed2c4b956601b
ALAN NATANIEL JOLODOVSKY <46830988@ort>
2023-04-10 10:39:14 -0300

    definitivo 2.0

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
+       List<int> listaDNI = new List<int>();
        List<Persona> listaPersonas = new List<Persona>();
        int Opcion = 0;
        int contador = 0;
@@ -10,6 +11,7 @@ class Program
        int contador2 = 0;
        bool Vota;
        int suma = 0;
+       int ABuscar;
        while (Opcion != 5){
         Console.WriteLine("1- Cargar Nueva Persona \n 2- Obtener Estadísticas del Censo \n 3- Buscar Persona \n 4- Modificar Mail de una Persona. \n 5- Salir");
         Opcion = IngresarInt("Ingrese la opción");
@@ -31,14 +33,56 @@ class Program
        if (Vota = true){
        contador2 ++;
        }
+       listaDNI.Add(Persona1.DNI);
        break;
        case 2:
        double Promedio = suma / contador;
        Console.WriteLine("Estadísticas del censo: \n Cantidad de Personas: " + contador + " \n Cantidad de Personas habilitadas para votar: "+ contador2 + " \n Promedio de Edad: " + Promedio);
        break;
        case 3:
+       ABuscar = IngresarInt("Ingrese un DNI para buscar: ");
+       int posicion = listaDNI.IndexOf(ABuscar);
+       if(posicion > -1){
+         Console.WriteLine("La persona con el dni " + ABuscar + " está registrada como: " + listaPersonas[posicion].Nombre + " " + listaPersonas[posicion].Apellido + " " + listaPersonas[posicion].DNI +" "+ listaPersonas[posicion].Email+" " +listaPersonas[posicion].FechaNacimiento);
+         calculo = listaPersonas[posicion].ObtenerEdad(listaPersonas[posicion].FechaNacimiento);
+         Vota = listaPersonas[posicion].PuedeVotar(calculo);
+         if (Vota = true){
+            Console.WriteLine("Puede votar");
+         }
+         else{
+            Console.WriteLine("No puede votar aún");
+         }
+       }
+       else{
+        Console.WriteLine("No se encontró al individuo, por favor ingreselo en la opción 1 del menú");
+         Console.Write("Restarting menu below an critical error");
+        System.Threading.Thread.Sleep(1000);
+        Console.Write(".");
+        System.Threading.Thread.Sleep(1000);
+        Console.Write(".");
+        System.Threading.Thread.Sleep(1000);
+        Console.Write(".");
+        System.Threading.Thread.Sleep(1000);
+        Console.Clear();
+       }
        break;
        case 4:
+        ABuscar = IngresarInt("Ingrese un DNI para buscar y lograr modificar el mail: ");
+        Console.Write("Buscando DNI");
+        System.Threading.Thread.Sleep(1000);
+        Console.Write(".");
+        System.Threading.Thread.Sleep(1500);
+        Console.Write(".");
+        System.Threading.Thread.Sleep(1200);
+        Console.Write(".");
+        System.Threading.Thread.Sleep(2000);
+        Console.Clear();
+        posicion = listaDNI.IndexOf(ABuscar);
+        if(posicion > -1){
+          Console.WriteLine("La persona con el dni " + ABuscar + " Tiene el mail: " + listaPersonas[posicion].Email);
+          string newMail = IngresarString("Ingresá el nuevo mail: ");
+          listaPersonas[posicion].Email = newMail;
+        }
        break;
         }
         }
